b35ab0b6d2b68559a596bdd12848536632c6e779
Jay Rovacsek <jay@rovacsek.com>
2022-12-14 09:00:24 +1100

    Minor fixes to resolve array vs set

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
+{ pkgs ? import <nixpkgs> { } }:
 let
+  inherit (pkgs.lib) recursiveUpdate;
   systems = [ "aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
   config = system:
     (import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
@@ -7,4 +9,5 @@ let
         "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
       sha256 = lock.nodes.flake-compat.locked.narHash;
     }) { src = ./.; }).defaultNix.outputs.packages.${system};
-in builtins.map (system: (config system)) systems
+in builtins.foldl' (x: y: (recursiveUpdate x y)) { }
+(builtins.map (system: (config system)) systems)
